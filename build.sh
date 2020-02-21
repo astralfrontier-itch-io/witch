99ec71fcda7cc022d927e5bdcc05c1f040a0b7d3
Bill Garrett <garrett@astralfrontier.org>
2020-02-21 00:14:35 -0800

    Build script and formatting changes

--- build.sh
+++ build.sh
@@ -4,40 +4,25 @@ set -xeuo pipefail
 
 # apt install pandoc texlive-base texlive-latex-recommended texlive texlive-latex-extra texlive-xetex
 
-# Usage: runPandoc $INPUT $OUTPUT $TEMPLATE
-function runPandocLocal() {
-    local INPUT=${1}
-    local OUTPUT=${2}
-    local TEMPLATE=${3:-default}
-    pandoc "$INPUT"             \
-        -o "$OUTPUT"            \
-        --data-dir pandoc       \
-        --template "$TEMPLATE"  \
-        --latex-engine=xelatex  \
-        -f gfm                  \
-        #
-}
-
-function runPandocDocker() {
-    local INPUT=${1}
-    local OUTPUT=${2}
-    local TEMPLATE=${3:-default}
-    docker run --rm -v $PWD:/source astralfrontier/pandoc:2.9.2 \
-        "$INPUT"                \
-        -o "$OUTPUT"            \
-        --data-dir pandoc       \
-        --template "$TEMPLATE"  \
-        --pdf-engine=xelatex  \
-        -f gfm                  \
-        #
-}
+export PANDOC=$( which pandoc )
+if [ -z "$PANDOC" ]; then
+    which docker && { export PANDOC="docker run --rm -v $PWD:/source astralfrontier/pandoc:2.9.2"; }
+fi
+[ -n "$PANDOC" ] || { echo "No pandoc found!"; exit 1; }
 
 # Usage: runPandoc input-file priority template
 function runPandoc() {
     local INPUT=${1}
     local OUTPUT_BASE=${INPUT##*/}
     local OUTPUT=dist/$2${OUTPUT_BASE%.md}.pdf
-    runPandocDocker "$INPUT" "$OUTPUT" "$3"
+    local TEMPLATE=${3:-default}
+    $PANDOC "$INPUT"            \
+        -o "$OUTPUT"            \
+        --data-dir pandoc       \
+        --template "$TEMPLATE"  \
+        --pdf-engine=xelatex    \
+        -f markdown             \
+        # 
 }
 
 [ -d dist ] || { mkdir dist; }
