61fd1944d1a657ff69f05bed8a3d8929fa0f1125
Bill Garrett <garrett@astralfrontier.org>
2020-02-20 16:30:50 -0800

    Run Pandoc from docker

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -xeuo pipefail
 # apt install pandoc texlive-base texlive-latex-recommended texlive texlive-latex-extra texlive-xetex
 
 # Usage: runPandoc $INPUT $OUTPUT $TEMPLATE
-function runPandoc() {
+function runPandocLocal() {
     local INPUT=${1}
     local OUTPUT=${2}
     local TEMPLATE=${3:-default}
@@ -14,12 +14,37 @@ function runPandoc() {
         --data-dir pandoc       \
         --template "$TEMPLATE"  \
         --latex-engine=xelatex  \
-        #-f gfm                  \
+        -f gfm                  \
         #
 }
 
-runPandoc ./rules/rules.md rules.pdf default
-runPandoc ./playbooks/idiot.md idiot.pdf playbook
+function runPandocDocker() {
+    local INPUT=${1}
+    local OUTPUT=${2}
+    local TEMPLATE=${3:-default}
+    docker run --rm -v $PWD:/source astralfrontier/pandoc:2.9.2 \
+        "$INPUT"                \
+        -o "$OUTPUT"            \
+        --data-dir pandoc       \
+        --template "$TEMPLATE"  \
+        --pdf-engine=xelatex  \
+        -f gfm                  \
+        #
+}
+
+# Usage: runPandoc input-file priority template
+function runPandoc() {
+    local INPUT=${1}
+    local OUTPUT_BASE=${INPUT##*/}
+    local OUTPUT=dist/$2${OUTPUT_BASE%.md}.pdf
+    runPandocDocker "$INPUT" "$OUTPUT" "$3"
+}
+
+[ -d dist ] || { mkdir dist; }
+rm -f dist/*
+
+find rules -type f | while read i; do runPandoc $i B default; done
+find playbooks -type f | while read i; do runPandoc $i P playbook; done
 
 # Combine PDFs
 gs -sDEVICE=pdfwrite            \
@@ -30,7 +55,4 @@ gs -sDEVICE=pdfwrite            \
     -dCompressFonts=true        \
     -r150                       \
     -sOutputFile=WITCH.pdf      \
-    rules.pdf                   \
-    idiot.pdf
-
-cp WITCH.pdf /mnt/c/Users/opens/Downloads
+    dist/*.pdf
